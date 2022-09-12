ea4c286eb38c90619c3533687e2e6576a0288199
mert <mert.ma346@gmail.com>
2022-09-12 15:24:10 +0300

    Initial Commit

---
+++ swiftUI-Preview/swiftUI_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_PreviewApp.swift
+//  swiftUI-Preview
+//
+//  Created by Mert AKBAŞ on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
